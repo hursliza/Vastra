ccfb0da761ad13c94717be3cbcef76fa760db485
alicjak1519 <akrzeminska1519@gmail.com>
2021-04-21 17:48:50 +0200

    Fix bug with displaying map only with 0 index

--- app/src/main/java/com/io/vastra/history/historyItem/HistoryItemMapFragment.kt
+++ app/src/main/java/com/io/vastra/history/historyItem/HistoryItemMapFragment.kt
@@ -24,8 +24,10 @@ class HistoryItemMapFragment : Fragment() {
 
     private lateinit var mapView: MapView
 
-    private val viewModel: HistoryItemViewModel by viewModels({ requireParentFragment() }) {
-        HistoryItemViewModelFactory(viewLifecycleOwner);
+    val viewModel: HistoryItemViewModel by viewModels() {
+        val idx = parentFragment?.arguments?.getInt(HistoryDetailsStatisticsArgs.RunIdx.name)
+            ?: throw InstantiationError("Cannot create details when run index is not provided")
+        HistoryItemViewModelFactory(viewLifecycleOwner, idx)
     }
 
     override fun onCreateView(
