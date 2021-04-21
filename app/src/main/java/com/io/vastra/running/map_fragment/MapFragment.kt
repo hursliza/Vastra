dbb799dc802f83fcd165560d4033f288c6e228d1
alicjak1519 <akrzeminska1519@gmail.com>
2021-04-21 21:35:44 +0200

    Add custom marker

--- app/src/main/java/com/io/vastra/running/map_fragment/MapFragment.kt
+++ app/src/main/java/com/io/vastra/running/map_fragment/MapFragment.kt
@@ -8,28 +8,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
-
 import com.io.vastra.R
-import com.io.vastra.data.entities.RoutePoint
 import com.io.vastra.running.running_view_model.RunViewModelState
 import com.io.vastra.running.running_view_model.RunningViewModel
 import com.io.vastra.running.running_view_model.RunningViewModelFactory
+import com.mapbox.mapboxsdk.annotations.IconFactory
 import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.annotations.PolylineOptions
-import kotlin.time.ExperimentalTime
-
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import kotlin.time.ExperimentalTime
 
 
 @ExperimentalTime
 class MapFragment : Fragment() {
 
     private lateinit var mapView: MapView
+    private val MAKI_ICON_AIRPORT = "airport-15"
+
     private var state: RunViewModelState = RunViewModelState.InActive;
     private val viewModel: RunningViewModel by viewModels({ requireParentFragment() }) {
         RunningViewModelFactory();
@@ -50,38 +52,45 @@ class MapFragment : Fragment() {
 
     private fun configureSubscriptions() {
         viewModel.state.observe(viewLifecycleOwner) {
-            state = it;
+            state = it
         }
 
         viewModel.currentLocation.observe(viewLifecycleOwner) {
             mapView.getMapAsync { mapboxMap ->
+                val actualRoutePoint = viewModel.currentLocation.value!!
+                val actualPosition = LatLng(actualRoutePoint.lat, actualRoutePoint.long)
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS)
+                updateCameraPosition(mapboxMap, actualPosition)
+                updateMapMarker(mapboxMap, actualPosition)
 
                 when (state) {
                     RunViewModelState.Active -> {
-                        updateMapMarker(mapboxMap)
                         updateMapPolyline(mapboxMap)
                     }
-                    else -> updateMapMarker(mapboxMap)
                 }
             }
         }
     }
 
-    private fun updateMapMarker(mapboxMap: MapboxMap) {
-        mapboxMap.setStyle(Style.MAPBOX_STREETS)
-        val actualRoutePoint = viewModel.currentLocation.value!!
-        val actualPosition = LatLng(actualRoutePoint.lat, actualRoutePoint.long)
+    private fun updateCameraPosition(mapboxMap: MapboxMap, actualPosition: LatLng) {
         val cameraPosition: CameraPosition = CameraPosition.Builder()
             .target(actualPosition) // set the camera's center position
             .zoom(15.0) // set the camera's zoom level
             .build()
         mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
 
+    }
+
+    private fun updateMapMarker(mapboxMap: MapboxMap, actualPosition: LatLng) {
+        val iconFactory = IconFactory.getInstance(mapView.context)
+        val icon = iconFactory.fromResource(R.drawable.mapbox_compass_icon)
+
         mapboxMap.clear()
         mapboxMap.addMarker(
             MarkerOptions()
                 .position(LatLng(actualPosition))
-                .title("Your actual location")
+                .icon(icon)
         )
     }
 
